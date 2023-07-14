693953d33826aa8d3b5ded1777836c1466d143b1
yangchlin <62425061+yangchlin@users.noreply.github.com>
2023-07-14 22:32:16 +0800

    Update K2P.sh

--- monw/scripts/K2P.sh
+++ monw/scripts/K2P.sh
@@ -4,9 +4,9 @@
 # 修改默认参数（不同设备拷贝到相应 *.sh)          by: TurBoTse
 #####################################################################
 
-lan_ip="192.168.6"                          # LAN 地址 别写后面的 .1
+lan_ip="192.168.0"                          # LAN 地址 别写后面的 .1
 default_path="./user/shared"                # 默认配置路径
-config="./configs/templates/K2P.config"     # 默认配置文件
+config="./configs/templates/PSG1218.config"     # 默认配置文件
 
 echo "修改 LAN IP 地址"
 sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
